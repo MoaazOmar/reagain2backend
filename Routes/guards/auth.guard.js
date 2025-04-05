35b7ffcdbe82d77a31601369141aa11e35806169
MoaazOmar <133528519+MoaazOmar@users.noreply.github.com>
2025-04-05 10:48:21 +0200

    modifyiingSecurity

--- Routes/guards/auth.guard.js
+++ Routes/guards/auth.guard.js
@@ -1,9 +1,14 @@
-exports.preventAccessLogin   = (req,res,next)=>{
- if(!req.session.user) next()
- else res.redirect('/')
-}
+exports.preventAccessLogin = (req, res, next) => {
+    if (!req.session.user) next();
+    else res.redirect('/');
+};
 
-exports.isLoggedIn = (req , res , next)=>{
-    if(req.session.user) next()
-    else res.redirect('/login')
-}
+exports.isLoggedIn = (req, res, next) => {
+    if (req.session.user) {
+        console.log('User authenticated:', req.session.user);
+        next();
+    } else {
+        console.log('User not authenticated, blocking access');
+        res.status(401).json({ message: 'Unauthorized, please log in' });
+    }
+};
