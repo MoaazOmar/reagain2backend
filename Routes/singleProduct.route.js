81aed9748fa301f92dca870f5e869e575b55e3b6
MoaazOmar <133528519+MoaazOmar@users.noreply.github.com>
2025-04-06 10:16:12 +0200

    editingAndFixin3g

--- Routes/singleProduct.route.js
+++ Routes/singleProduct.route.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { getSingleProduct, getRelatedProductsExcludingSelectedProduct, toggleDislikeProduct, toggleLikeProduct } = require('../Controllers/products.controller');
-const { authGuard } = require('./guards/auth.guard');
+const authGuard = require('./guards/auth.guard');
 
 router.get('/:id', getSingleProduct);
 router.get('/:id/related', getRelatedProductsExcludingSelectedProduct);
