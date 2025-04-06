1a8b06bbb6cf9a940f9213c2fa4369ccdba5d021
MoaazOmar <133528519+MoaazOmar@users.noreply.github.com>
2025-04-06 10:11:03 +0200

    editingAndFixin2g

--- Routes/singleProduct.route.js
+++ Routes/singleProduct.route.js
@@ -4,8 +4,8 @@ const { authGuard } = require('./guards/auth.guard');
 
 router.get('/:id', getSingleProduct);
 router.get('/:id/related', getRelatedProductsExcludingSelectedProduct);
-router.post('/:id/like', isLoggedIn, toggleLikeProduct);
-router.post('/:id/dislike', isLoggedIn, toggleDislikeProduct);
+router.post('/:id/like', authGuard.isLoggedIn, toggleLikeProduct);
+router.post('/:id/dislike', authGuard.isLoggedIn, toggleDislikeProduct);
 
 router.get('/test', (req, res) => {
     res.status(200).json({ message: 'Single Product route is working' });
